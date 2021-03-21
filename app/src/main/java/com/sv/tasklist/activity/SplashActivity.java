f74856697b7cf85918fdd588b9b20f8accca1904
sergeybelyakov <belyakovsergeyrus@yandex.ru>
2021-03-21 22:52:18 +0300

    UPD: Добавлен SplashActivity (2)

--- app/src/main/java/com/sv/tasklist/activity/SplashActivity.java
+++ app/src/main/java/com/sv/tasklist/activity/SplashActivity.java
@@ -3,21 +3,62 @@ package com.sv.tasklist.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sv.tasklist.R;
 
+import static java.lang.Thread.sleep;
+
 public class SplashActivity extends AppCompatActivity {
+
+    ProgressBar mProgressBar;
+    TextView tvPercent;
+    int mProgress = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(() -> {
+        mProgressBar = findViewById(R.id.progressBar1);
+        tvPercent = findViewById(R.id.value123);
+
+        new Thread(myThread).start();
+
+//        new Handler().postDelayed(() -> {
+//            startActivity(new Intent(SplashActivity.this,
+//                    MainActivity.class));
+//            finish(); }, 2000);
+    }
+
+    private final Runnable myThread = new Runnable() {
+        @Override
+        public void run() {
+            while (mProgress < 100) {
+                try {
+                    myHandler.sendMessage(myHandler.obtainMessage());
+                    sleep(5);
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
+                new Handler().post(() -> tvPercent.setText(String.valueOf(mProgress)));
+            }
             startActivity(new Intent(SplashActivity.this,
                     MainActivity.class));
-            finish(); }, 2000);
-    }
+            finish();
+        }
+
+        Handler myHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                mProgress++;
+                mProgressBar.setProgress(mProgress);
+                tvPercent.setText(String.valueOf(mProgress));
+            }
+        };
+    };
 }
